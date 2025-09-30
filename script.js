6ab5df03264b0ad8cde92c20f5d1a1269ca7ea22
anuhang9 <anuhang88@gmail.com>
2025-09-30 15:09:25 +0545

    fied the bug about next song and add some other songs. you can read readme file.

--- script.js
+++ script.js
@@ -20,43 +20,101 @@ let song_store = [
     name: "Bhanai",
     artist: "Tribal Rain",
     album: "Roka Yo Samay",
-    image: "roka_yo_samay.jpg",
-    song: "Bhanai.mp3",
+    image: "./assets/img/roka_yo_samay.jpg",
+    song: "./assets/songs/Bhanai.mp3",
   },
   {
     name: "Laijaw Malai",
     artist: "Tribal Rain",
     album: "Roka Yo Samay",
-    image: "roka_yo_samay.jpg",
-    song: "Laijaw_Malai.mp3",
+    image: "./assets/img/roka_yo_samay.jpg",
+    song: "./assets/songs/Laijaw_Malai.mp3",
   },
   {
     name: "Roka Yo Samay",
     artist: "Tribal Rain",
     album: "Roka Yo Samay",
-    image: "roka_yo_samay.jpg",
-    song: "Roka_Yo_Samay.mp3",
+    image: "./assets/img/roka_yo_samay.jpg",
+    song: "./assets/songs/Roka_Yo_Samay.mp3",
   },
   {
     name: "Sahara",
     artist: "Tribal Rain",
     album: "Sahara",
-    image: "sahara.jpg",
-    song: "Sahara.mp3",
+    image: "./assets/img/sahara.jpg",
+    song: "./assets/songs/Sahara.mp3",
+  },
+  {
+    name: "Traditional Swag",
+    artist: "Tribal Rain",
+    album: "Roka Yo Samay",
+    image: "./assets/img/roka_yo_samay.jpg",
+    song: "./assets/songs/TraditionalSwang.mp3",
+  },
+  {
+    name: "Devi Deuta",
+    artist: "Tribal Rain",
+    album: "Roka Yo Samay",
+    image: "./assets/img/roka_yo_samay.jpg",
+    song: "./assets/songs/DeviDeuta.mp3",
+  },
+  {
+    name: "Jiunu Nai Hola",
+    artist: "Tribal Rain",
+    album: "Roka Yo Samay",
+    image: "./assets/img/roka_yo_samay.jpg",
+    song: "./assets/songs/JiunuNaiHola.mp3",
+  },
+  {
+    name: "Khola",
+    artist: "Tribal Rain",
+    album: "Roka Yo Samay",
+    image: "./assets/img/roka_yo_samay.jpg",
+    song: "./assets/songs/Khola.mp3",
+  },
+  {
+    name: "Swarga",
+    artist: "Tribal Rain",
+    album: "Roka Yo Samay",
+    image: "./assets/img/roka_yo_samay.jpg",
+    song: "./assets/songs/Swarga.mp3",
+  },
+  {
+    name: "Chinta",
+    artist: "Tribal Rain",
+    album: "Roka Yo Samay",
+    image: "./assets/img/roka_yo_samay.jpg",
+    song: "./assets/songs/Chinta.mp3",
+  },
+  {
+    name: "Sunyata",
+    artist: "Tribal Rain",
+    album: "Roka Yo Samay",
+    image: "./assets/img/roka_yo_samay.jpg",
+    song: "./assets/songs/Sunyata.mp3",
+  },
+  {
+    name: "Narisauna",
+    artist: "Tribal Rain",
+    album: "Roka Yo Samay",
+    image: "./assets/img/roka_yo_samay.jpg",
+    song: "./assets/songs/Narisawna.mp3",
+  },
+  {
+    name: "Kura",
+    artist: "Tribal Rain",
+    album: "Roka Yo Samay",
+    image: "./assets/img/roka_yo_samay.jpg",
+    song: "./assets/songs/Kura.mp3",
   },
 ];
 
-
-
-
 let currentIndex = song_store.findIndex((song) => song.name === "Bhanai");
-console.log(currentIndex)
 
 // Function to load a song
 function loadSong(index) {
   const currentSong = song_store[index];
-  if (play_song)
-  play_song.pause(); // Pause the current song if playing
+  if (play_song) play_song.pause(); // Pause the current song if playing
   play_song = new Audio(currentSong.song); // Set the new song
   song_name.innerText = currentSong.name;
   song_artist.innerText = currentSong.artist;
@@ -65,23 +123,35 @@ function loadSong(index) {
 
   play_song.addEventListener("loadedmetadata", () => {
     song_time_range.max = play_song.duration;
-    song_timing_total.innerText = `${Math.floor(play_song.duration / 60)}:${Math.floor(
-      play_song.duration % 60
-    )}`;
+    song_timing_total.innerText = `${Math.floor(
+      play_song.duration / 60
+    )}:${Math.floor(play_song.duration % 60)}`;
   });
 
   play_song.addEventListener("timeupdate", () => {
     song_time_range.value = play_song.currentTime;
     const minutes = Math.floor(play_song.currentTime / 60);
     const seconds = Math.floor(play_song.currentTime % 60);
-    song_timing_current.innerText = `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+    song_timing_current.innerText = `${minutes}:${
+      seconds < 10 ? "0" : ""
+    }${seconds}`;
   });
+
+  // song next after finish
+  let intervalId = setInterval(() => {
+    if (song_timing_current.innerText >= song_timing_total.innerText) {
+      currentIndex = (currentIndex + 1) % song_store.length; // Loop back to start if at the end
+      loadSong(currentIndex);
+      if (!is_playing) playLoadedSong();
+      clearInterval(intervalId);
+    }
+  }, 1000);
 }
 
 // Function to play the loaded song
 function playLoadedSong() {
   play_song.play();
-  song_play_pause_img.src = "pause.png";
+  song_play_pause_img.src = "./assets/img/pause.png";
   song_image.style.animationName = "songimg";
   song_image.style.animationPlayState = "running";
   is_playing = false;
@@ -96,7 +166,7 @@ song_play_pause.addEventListener("click", () => {
     playLoadedSong();
   } else {
     play_song.pause();
-    song_play_pause_img.src = "play.png";
+    song_play_pause_img.src = "./assets/img/play.png";
     song_image.style.animationPlayState = "paused";
     is_playing = true;
   }
@@ -121,10 +191,28 @@ song_time_range.addEventListener("input", () => {
   play_song.currentTime = song_time_range.value;
   const currentMinutes = Math.floor(play_song.currentTime / 60);
   const currentSeconds = Math.floor(play_song.currentTime % 60);
-  song_timing_current.innerText = `${currentMinutes}:${currentSeconds < 10 ? "0" : ""}${currentSeconds}`;
+  song_timing_current.innerText = `${currentMinutes}:${
+    currentSeconds < 10 ? "0" : ""
+  }${currentSeconds}`;
 });
 
 // Repeat button functionality
 song_repeat.addEventListener("click", () => {
   play_song.currentTime = 0;
 });
+
+
+// music library button
+
+let library_button = document.querySelector('.library');
+library_button.innerHTML = "︿"
+// let library_button_current = "﹀";
+
+library_button.addEventListener("click",(evt)=>{
+  if(library_button.innerHTML === "︿"){
+    library_button.innerHTML= '﹀'
+  }
+  else{
+    library_button.innerHTML="︿"
+  }
+})
